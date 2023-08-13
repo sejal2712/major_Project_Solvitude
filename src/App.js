8f4f1191f590db4ff90320bb34018bdbdfa4d0f0
Shrishti20706 <shrishtijain20706@acropolis.in>
2023-08-14 03:14:29 +0530

    servicepage added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import BodySec from './components/BodySec';
 import Navbar from './components/Navbar';
-
+import Card from './components/basics/Card'
 function App() {
   return (
     <>
     <Navbar />
     <BodySec />
+    <Card/>
     </>
   );
 }
---
+++ src/components/basics/cardAPI.js
@@ -0,0 +1,46 @@
+const Api=[{
+  id:1,
+  serviceName:"EC2",
+  image:"images/img1.jpg",
+  category:"Compute",
+  description:"Amazon EC2 is a web service that provides resizable compute capacity in the cloud. It is designed to make web-scale computing easier for developers."
+},{
+id:2,
+serviceName:"Amazon EBS",
+image:"images/img4.jpg",
+category:"Storage",
+description:"Amazon EBS allows you to create storage volumes and attach them to Amazon EC2 instances,you can create a file system on top of these volumes or run a database."
+},
+{
+id:3,
+  serviceName:"Amazon RDS",
+  image:"images/img3.jpg",
+  category:"Database",
+  description:"Amazon Relational Database Service (Amazon RDS) is a service used to manage database engines and instances, including Amazon Aurora databases."
+},
+{
+id:4,
+  
+
+  serviceName:"Amazon Cloudfront",
+  image:"images/img2.jpg",
+  category:"Network and Content Delivery",
+  description:"Amazon CloudFront is a web service that speeds up distributio static and dynamic web content,to your users. CloudFront delivers your content through edge locations."
+},
+{
+  id:5,
+    serviceName:"Amazon GuardDuty",
+    image:"images/img5.jpg",
+    category:"Security",
+    description:"a threat detection service that continuously monitors your AWS accounts and workloads for malicious activity and delivers detailed security findings for visibility and remediation."
+  },
+  {
+    id:6,
+      serviceName:"Amazon Cloudwatch",
+      image:"images/img6.jpg",
+      category:"Management",
+      description:"Amazon EBS allows you to create storage volumes and attach them to Amazon EC2 instances. Once attached, you can create a file system on top of these volumes, run a database."
+    }
+]
+
+export default Api;
