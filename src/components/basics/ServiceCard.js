6afebf87e541bad03a4d6daa28df34de0522a836
Shrishti20706 <shrishtijain20706@acropolis.in>
2023-11-19 01:58:31 +0530

    deploy

--- src/components/basics/ServiceCard.js
+++ src/components/basics/ServiceCard.js
@@ -1,54 +1,76 @@
-import React, { useState } from 'react'
-import "./ServiceStyle.css"
-import cardAPI from "./ServiceCardAPI.js"
-import ServiceCard from "./ServiceCardUI"
-import NavServices from './NavServices'
+import React, { useState } from 'react';
+import "./ServiceStyle.css";
+import cardAPI from "./ServiceCardAPI.js";
+import ServiceCardUI from "./ServiceCardUI";
+import NavServices from './NavServices';
+
 const uniqueService = [...new Set(cardAPI.map((curEle) => {
   return curEle.category;
+})), "All"];
 
-})),
-  "All",
-];
-
-const Service = () => {
-  const [cardData, setcardData] = useState(cardAPI);
-
+const ServiceCard = () => {
+  const [cardData, setCardData] = useState(cardAPI);
   const [serviceList, setServiceList] = useState(uniqueService);
 
   const filterService = (category) => {
     if (category === "All") {
-      return setcardData(cardAPI);
-
-
+      return setCardData(cardAPI);
     }
     const updatedService = cardAPI.filter((curEle) => {
       return curEle.category === category;
     });
-    setcardData(updatedService);
+    setCardData(updatedService);
   };
-  
 
-
+  const handleDeployClick = () => {
+    // Replace the placeholders with your actual CloudFormation console URL
+    const cloudFormationConsoleURL = "https://eu-north-1.console.aws.amazon.com/cloudformation/home?region=eu-north-1";
+    window.open(cloudFormationConsoleURL, '_blank');
+  };
 
   return (
     <>
       <section id="services"> 
-      <h1 style={{ textAlign:"center"}}><b>OUR SERVICES</b></h1>
-      <NavServices filterService={filterService} serviceList={serviceList} />
-      
+        <h1 style={{ textAlign: "center" }}><b>OUR SERVICES</b></h1>
+        <NavServices filterService={filterService} serviceList={serviceList} />
 
-      
-        <div style={{ background: "linear-gradient(#17cf97, #1b2430)",marginTop:"0px" }}>
-          <ServiceCard cardData={cardData} />
-
+        <div style={{ background: "linear-gradient(#17cf97, #1b2430)", marginTop: "0px" }}>
+          <ServiceCardUI cardData={cardData} />
         </div>
-
+        <br/>
+        <br/>
+        <h1 style={{ textAlign: "center" }}><b>Deployment Solutions for Your Success</b></h1>
+        <div className="maindiv">
+          <div className="dpcontainer">
+            <div className="image-container">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1150px-React-icon.svg.png" alt="Image Alt Text"/>
+            </div>
+            <div className="content-container">
+              <h2 className='dphead'>ReactJS</h2>
+              <p>
+                React.js is a JavaScript library for building user interfaces.
+                It enables the creation of reusable UI components for efficient development.
+              </p>
+              <button className="deploybtn" onClick={() => handleDeployClick()}>Click to deploy &rarr;</button>
+            </div>
+          </div>
+          <div className="dpcontainer">
+            <div className="image-container">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/2560px-Node.js_logo.svg.png" alt="Image Alt Text"/>
+            </div>
+            <div className="content-container">
+              <h2 className='dphead'>NodeJS</h2>
+              <p className='dppara'>
+                Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine.
+                It allows you to build scalable network applications easily.
+              </p>
+              <button className="deploybtn" onClick={() => handleDeployClick()}>Click to deploy &rarr;</button>
+            </div>
+          </div>
+        </div>
       </section>
-
     </>
-  )
-}
+  );
+};
 
-
-
-export default Service
+export default ServiceCard;
